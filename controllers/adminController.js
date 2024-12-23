6a0881c9a4953b9a6047c86b9ae23263ee54f138
Dhairy-Dobariya-swiftrut <dhairy.dobariya@swiftrut.com>
2024-12-24 03:57:08 +0530

    fixes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-import User from '../models/User.model.js';
+import UserModel from '../models/User.model.js';
 import sendResponse from '../services/response.services.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
