56ef5053d12fd14d9bf6ec9654b8bee486599ca9
priyanka-swiftrut <priyanka.dudhat@swiftrut.com>
2024-12-24 00:10:42 +0530

    basic_setup done

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import connectDB from "./config/db.js";
+import API from "./routes/index.js"
+// import passportConfig from "./config/passport.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+// app.use(session({
+//   secret: process.env.COOKIE_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: { maxAge: 60000 },
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+connectDB();
+
+// passportConfig(passport);
+
+app.use("/user",API );
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
