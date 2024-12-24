7b69bb19dab0da4f246683ffb6290d9f1f8ba843
Dishant106 <dishant.madariya@swiftrut.com>
2024-12-25 00:19:18 +0530

    feat: add RegisterDoctor method and route for doctor registration

--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,6 @@ const adminController = new AdminController();
 
 router.post("/editAdmin", upload.fields([{ name: 'profilePicture', maxCount: 1 }]), adminController.EditProfile.bind(adminController));
 router.delete("/delete/:id", adminController.deleteProfile.bind(adminController));
+router.post('/registerDoctor', upload.fields([{ name: 'profilePicture', maxCount: 1 }]), adminController.RegisterDoctor.bind(adminController));
 
 export default router;
