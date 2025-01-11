5a938e97e0da28600dcfeedc399edee7876416ac
Dishant106 <dishant.madariya@swiftrut.com>
2025-01-11 20:13:05 +0530

    refactor: remove console logs from appointment, chat, notification, and socket controllers

--- src/socket/notificationSocket.js
+++ src/socket/notificationSocket.js
@@ -1,13 +1,9 @@
 const notification = (io) => {
     io.on('connection', (socket) => {
-        console.log('Client connected:', socket.id);
-
         // Join society room
         socket.on('join-hospital', (hospitalId) => {
             socket.join(`hospital-${hospitalId}`);
-            console.log(`Client joined hospital room: hospital-${hospitalId}`);
         });
-
         socket.on('disconnect', () => {
             console.log('Client disconnected:', socket.id);
         });
