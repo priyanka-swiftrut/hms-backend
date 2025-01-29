50a4e6db892d00c471a81f9a3fe399974e6b60f1
Dishant106 <dishant.madariya@swiftrut.com>
2025-01-29 21:22:14 +0530

    refactor: remove unused database connection, models, routes, and services

--- src/src/routes/rezorpay.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder, verifyPayment } from '../controllers/razoepayController.js';
-
-const router = express.Router();
-
-router.post('/create-order', createOrder);
-router.post('/verify-payment', verifyPayment);
-
-export default router;
